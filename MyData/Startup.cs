2cb8d3b55334c0427fb1af33b9f3372da88ae878
0004072 <kanchana@hsenidlanka.com>
2017-08-17 11:33:23 +0530

    Initial Commit. Functionality implemented for Bio Data display.

---
+++ MyData/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyData.Startup))]
+namespace MyData
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
